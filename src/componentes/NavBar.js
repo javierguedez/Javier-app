fca92245eeb4eec4d1b9169e5b97e5bb1690e317
guedezjavier <jvrgdz@gmail.com>
2022-02-28 23:57:01 -0500

    navbar

---
+++ src/componentes/NavBar.js
@@ -0,0 +1,13 @@
+function NavBar (){
+    return   <nav>
+      <label class="logo">Proyecto React</label>
+      <ul>
+          <li><a class="active" href="#">Inicio</a></li>
+          <li><a href="#">Menú</a></li>
+          <li><a href="#">Contactanos</a></li>
+          <li><a href="#">Quienes Somos</a></li>
+      </ul>
+  </nav>
+}
+
+export default NavBar
